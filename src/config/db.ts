f950bf852dc5d48acd6bfae0419007c01ac21682
Nguyễn Hiệp <hiepnguyen.250402@gmail.com>
2025-07-11 14:08:09 +0700

    upload(1) Zz:11:07:25

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,20 +1,18 @@
 import mongoose from "mongoose";
 require("dotenv").config();
 
-const MONGO_URI = process.env.MONGO_URI;
-
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI as string)
-        .then(() => {
-            console.log('Connected to MongoDB');
-        }).catch((error) => {
-            console.error('Error connecting to MongoDB:', error);
-        });
+    await mongoose.connect(process.env.MONGODB_URI as string, {
+      serverSelectionTimeoutMS: 5000, // Thử timeout 5 giây
+      socketTimeoutMS: 45000, // Thử socket timeout 45 giây
+    });
+    console.log("Connected to MongoDB");
   } catch (error) {
+    // Lỗi sẽ được bắt ở đây nếu mongoose.connect thất bại
     console.error("MongoDB connection error:", error);
-    process.exit(1); 
+    // process.exit(1); // Giữ lại dòng này để thoát ứng dụng nếu không kết nối được DB
   }
-}
+};
 
 export default connectDB;
